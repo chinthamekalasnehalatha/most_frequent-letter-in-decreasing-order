b9dfa04acd81a22edb301c222d185a78a4eafc1e
chinthamekalasnehalatha <85674711+chinthamekalasnehalatha@users.noreply.github.com>
2021-07-09 14:18:10 +0000

    Create Task.py

---
+++ Task.py
@@ -0,0 +1,11 @@
+S= input("enter a string")
+def most_frequent(string):
+    d = dict()
+    for i in string:
+        if i not in d:
+            d[i] = 1
+        else:
+            d[i] += 1
+    return d
+
+print (most_frequent(S))
